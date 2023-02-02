2a940ffeab0b86ca1fa808f53252da56340f94bc
Menachem Neiman <nerevar123@gmail.com>
2023-02-03 01:20:56 +0200

    setup dev enviroment

--- config/admin.ts
+++ config/admin.ts
@@ -1,8 +1,13 @@
 export default ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
+	auth: {
+		secret: env("ADMIN_JWT_SECRET"),
+	},
+	apiToken: {
+		salt: env("API_TOKEN_SALT"),
+	},
+	rateLimit: {
+		interval: { hour: 1, min: 30 },
+		timeWait: 3 * 1000,
+		max: 10,
+	},
 });
