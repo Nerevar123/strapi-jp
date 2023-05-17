b0cefcd3504122d54d01ac3c856f888a3d167240
Menachem Neiman <ext-menachem.neiman@placer.io>
2023-05-17 22:44:31 +0300

    update deps

--- config/admin.ts
+++ config/admin.ts
@@ -5,6 +5,11 @@ export default ({ env }) => ({
 	apiToken: {
 		salt: env('API_TOKEN_SALT'),
 	},
+	transfer: {
+		token: {
+			salt: env('TRANSFER_TOKEN_SALT'),
+		},
+	},
 	rateLimit: {
 		interval: { hour: 1, min: 30 },
 		timeWait: 3 * 1000,
